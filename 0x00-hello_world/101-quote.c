3d96e9709642364d32764c392b1b28e9edae59cb
shollytak <toremlog@gmail.com>
2022-12-08 12:33:25 -0800

    task_8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+nclude <stdio.h>
 #include <unistd.h>
 
 /**
- * main - prints exactly and that piece of art is useful
- *
- * Return: Always 0 (Success)
+ *   main - prints exactly and that piece of art is useful
+ *  
+ *   Return: Always 0 (Success)
  */
 int main(void)
 {
