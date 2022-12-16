c8fb6c03a0973e4b41db0d3670422098c4df659b
shollytak <toremlog@gmail.com>
2022-12-15 23:57:33 -0800

    task_0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,20 +6,10 @@
 int main(void)
 {
 	int i;
+	
 	i = 98;
+	postive_or_negative(i);
 
-	if (i < 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-	{
-		printf("%d is negative\n", i);
-	}
 	return (0);
 
 }
