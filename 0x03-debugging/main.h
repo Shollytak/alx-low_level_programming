ee80073dfe5a287561a70da63f12b5a23e660826
shollytak <toremlog@gmail.com>
2022-12-16 00:10:45 -0800

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,21 @@
-#ifndef MAIN_H
+fndef MAIN_H
+
 #define MAIN_H
+
 
 
 #include <stdio.h>
 
-void postive_or_negative(int i);
+
+
+void positive_or_negative(int i);
+
 int largest_number(int a, int b, int c);
+
 int convert_day(int month, int day);
+
 void print_remaining_days(int month, int day, int year);
+
 void print_times_tables(int n);
 
 
