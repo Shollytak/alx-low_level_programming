bc7b075529fe3659f85f5e9eb1af97a6c31bd314
shollytak <toremlog@gmail.com>
2023-01-12 02:53:55 -0800

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,15 @@
 int main(int argc, char *argv[])
 
 {
+	int a;
 	printf("argc = %d\n", argc);
 
+	if( a >  argc)
+	{
+	for (a = 0; a > argc; a++)
+	{
+		printf("argv[%d] = %s\n", a, argv[a]);
+	}
+	}
 	return (0);
 }
