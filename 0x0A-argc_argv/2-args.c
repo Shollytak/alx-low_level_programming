3e764b05403b01c0fee078565a4b4f06fe75f0d6
shollytak <toremlog@gmail.com>
2023-01-23 07:02:44 -0800

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include "main.h"
+/**
+  * main - print all argument
+  * @argc: argument count
+  * @argv: arguments
+  * Return: Always 0 (success)
+ */
 
 int main(int argc, char *argv[])
 {
