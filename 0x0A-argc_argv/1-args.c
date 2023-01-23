70259f012ac5421e62cc6632241c74f9867bdf4f
shollytak <toremlog@gmail.com>
2023-01-23 06:57:20 -0800

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+  * main - print the number of argument passed
+  * @argc: Count arguments
+  * @argv: Arguments
+  * Return: Always 0 (success)
+  */
 
 int main(int argc, char *argv[])
-
 {
 	(void)argv; /*Ignore argv*/
 	printf("%d\n", argc - 1);
